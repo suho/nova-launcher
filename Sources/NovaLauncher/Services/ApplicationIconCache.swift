f667795b6f5c9bca69d563865407eaed765cd3de
Su Ho <su.v@nimblehq.co>
2026-04-29 20:09:43 +0700

    Cache app icons during settings scrolling

--- Sources/NovaLauncher/Services/ApplicationIconCache.swift
+++ Sources/NovaLauncher/Services/ApplicationIconCache.swift
@@ -1,24 +1,45 @@
 import AppKit
 
-actor ApplicationIconCache {
+final class ApplicationIconCache {
     static let shared = ApplicationIconCache()
 
-    private var cache: [String: NSImage] = [:]
+    private let cache = NSCache<NSString, NSImage>()
+    private let inFlightQueue = DispatchQueue(label: "app.novalauncher.icon-cache.in-flight")
+    private var inFlightTasks: [String: Task<NSImage, Never>] = [:]
+
+    func cachedIcon(for url: URL) -> NSImage? {
+        cache.object(forKey: url.path as NSString)
+    }
 
     func icon(for url: URL) async -> NSImage {
         let path = url.path
+        let key = path as NSString
 
-        if let cachedIcon = cache[path] {
+        if let cachedIcon = cache.object(forKey: key) {
             return cachedIcon
         }
 
-        let icon = await Task.detached(priority: .utility) {
-            let image = NSWorkspace.shared.icon(forFile: path)
-            image.size = NSSize(width: 64, height: 64)
-            return image
-        }.value
+        let task = inFlightQueue.sync {
+            if let inFlightTask = inFlightTasks[path] {
+                return inFlightTask
+            }
 
-        cache[path] = icon
+            let task = Task.detached(priority: .utility) {
+                let image = NSWorkspace.shared.icon(forFile: path)
+                image.size = NSSize(width: 64, height: 64)
+                return image
+            }
+            inFlightTasks[path] = task
+            return task
+        }
+
+        let icon = await task.value
+
+        cache.setObject(icon, forKey: key)
+        inFlightQueue.sync {
+            inFlightTasks[path] = nil
+        }
+
         return icon
     }
 
--- Sources/NovaLauncher/Views/AppIconView.swift
+++ Sources/NovaLauncher/Views/AppIconView.swift
@@ -5,11 +5,21 @@ struct AppIconView: View {
     let url: URL
     let size: CGFloat
     @State private var icon: NSImage?
+    @State private var iconPath: String?
+
+    init(url: URL, size: CGFloat) {
+        self.url = url
+        self.size = size
+
+        let cachedIcon = ApplicationIconCache.shared.cachedIcon(for: url)
+        _icon = State(initialValue: cachedIcon)
+        _iconPath = State(initialValue: cachedIcon == nil ? nil : url.path)
+    }
 
     var body: some View {
         ZStack {
-            if let icon {
-                Image(nsImage: icon)
+            if let displayedIcon {
+                Image(nsImage: displayedIcon)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
             } else {
@@ -24,7 +34,28 @@ struct AppIconView: View {
         .frame(width: size, height: size)
         .accessibilityHidden(true)
         .task(id: url.path) {
-            icon = await ApplicationIconCache.shared.icon(for: url)
+            let path = url.path
+
+            if let cachedIcon = ApplicationIconCache.shared.cachedIcon(for: url) {
+                icon = cachedIcon
+                iconPath = path
+                return
+            }
+
+            icon = nil
+            iconPath = nil
+
+            let loadedIcon = await ApplicationIconCache.shared.icon(for: url)
+            icon = loadedIcon
+            iconPath = path
         }
+    }
+
+    private var displayedIcon: NSImage? {
+        if iconPath == url.path {
+            return icon
+        }
+
+        return ApplicationIconCache.shared.cachedIcon(for: url)
     }
 }
